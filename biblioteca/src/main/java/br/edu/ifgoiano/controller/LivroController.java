a8d3194c1cf8a9bd6dbf04813741aa14dacb36f0
nana <110903416+vanillabeetlee@users.noreply.github.com>
2022-10-04 09:07:00 -0300

    Add files via upload

---
+++ biblioteca/src/main/java/br/edu/ifgoiano/controller/LivroController.java
@@ -0,0 +1,41 @@
+package br.edu.ifgoiano.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.edu.ifgoiano.entidade.Livro;
+import br.edu.ifgoiano.servico.LivroService;
+import br.edu.ifgoiano.servico.LivroServiceImpl;
+
+@Controller
+public class LivroController {
+	
+    private final LivroService livroServiceImpl;
+
+    public LivroController(LivroServiceImpl livroServiceImpl) {
+        this.livroServiceImpl = livroServiceImpl;
+    }
+
+    @GetMapping("/livros")
+    public String listarLivros(Model model) {
+        model.addAttribute("livros", livroServiceImpl.listarLivros());
+        return "listar-livros";
+    } 
+    
+    @GetMapping("/livro/novo")
+    public String abrirNovoLivro(Model model) {
+    	
+    	Livro livro = new Livro();
+    	model.addAttribute("livro", livro);
+    	
+    	return "inserir-livro";
+    }
+    @PostMapping("/livros/inserir")
+    public String inserirLivro(Livro livro) {
+    	this.livroServiceImpl.inserir(livro); 
+    	
+    	return "redirect:/livros";
+    }
+}
---
+++ biblioteca/src/main/java/br/edu/ifgoiano/servico/LivroService.java
@@ -0,0 +1,12 @@
+package br.edu.ifgoiano.servico;
+
+import java.util.List;
+
+import br.edu.ifgoiano.entidade.Livro;
+
+public interface LivroService {
+	public List<Livro> listarLivros();
+
+	public void inserir(Livro livro);
+	
+}
